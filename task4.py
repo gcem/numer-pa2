e40fe323b0208f63de37abf3f90a4b51d7400d18
gcem <34423323+gcem@users.noreply.github.com>
2024-01-05 19:04:55 +0100

    reshape the subplots

--- task4.py
+++ task4.py
@@ -7,8 +7,8 @@ import numpy as np
 if __name__ == '__main__':
     imageByLabel = io.getDataByLabel(*io.getTrainingData())
 
-    two = imageByLabel[8][:1000]
-    six = imageByLabel[9][:1000]
+    two = imageByLabel[1][:1000]
+    six = imageByLabel[7][:1000]
 
     sample = np.concatenate([two, six], axis=0)
 
@@ -17,20 +17,20 @@ if __name__ == '__main__':
     subspaceCoordinates = task3.getSubspaceCoordinates(
         A, b, task2.stackImages(sample))
 
-    ax = plt.subplot(4, 4, (2, 12))
+    ax = plt.subplot(4, 8, (2, 4 + 8 * 2))
     plt.gcf().suptitle('Aufgabe 4')
 
     io.scatter2(subspaceCoordinates[:1000, :], subspaceCoordinates[1000:, :],
-                '8', '9')
+                '1', '7')
     ax = plt.gca()
     ax.legend()
     ax.set_title('Auf $H_2$ projizierte Koordinaten für die Ziffern 2 und 6')
 
-    ax = plt.subplot(4, 4, 15)  # middle of x axis
+    ax = plt.subplot(4, 8, 3 + 8 * 3)  # middle of x axis
     io.showImage(task3.vector2image(A[:, 0]),
                  '1. Hauptkomponent',
                  createFigure=False)
-    ax = plt.subplot(4, 4, 5)  # middle of y axis
+    ax = plt.subplot(4, 8, 1 + 8 * 1)  # middle of y axis
     io.showImage(task3.vector2image(A[:, 1]),
                  '2. Hauptkomponent',
                  createFigure=False)
